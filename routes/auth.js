d5df9ecbe426d56ffc54f507872ef504e411bfad
Nathaniel Simmons <nkl.simmons@proton.me>
2024-08-07 20:06:33 +0800

    Error handling

--- routes/auth.js
+++ routes/auth.js
@@ -10,65 +10,63 @@ function generateAccessToken(username) {
   return jwt.sign({username}, process.env.TOKEN_SECRET, { expiresIn: '86400s' });
 }
 
-function authenticateToken(req, res, next) {
-  // const authHeader = req.headers['authorization']
-  // const token = authHeader && authHeader.split(' ')[1]
-
-  // if (token == null) return res.sendStatus(401)
-
-  // jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-  //   console.log(err)
-
-  //   if (err) return res.sendStatus(403)
-
-  //   req.user = user
-
-  //   next()
-  // })
-}
-
 // function somethingsomethingpassword
 
+// TODO: Export these into service methods, which return promises
+// https://www.toptal.com/express-js/routes-js-promises-error-handling
 /* POST new user */
-router.post('/register', async function(req, res, next) {
+router.post('/register', async (req, res, next) => {
+  try {
+    const db = await dbConn;
 
-  const db = await dbConn;
+    const coll = db.collection("users");
 
-  const coll = db.collection("users");
+    const newUser = {
+      username: req.body.username,
+      password: req.body.password,
+    }
 
-  const newUser = {
-    username: req.body.username,
-    password: req.body.password,
+    coll.insertOne(newUser);
+
+    res.send({
+      id: newUser._id,
+      username: newUser.username,
+      token: generateAccessToken(newUser.username),
+    });
+  } catch(err) {
+    next(err);
   }
-
-  coll.insertOne(newUser)
-
-  res.send({
-    id: newUser._id,
-    username: newUser.username,
-    token: generateAccessToken(newUser.username),
-  });
 });
 
 /* POST retrieve JWT */
-router.post('/login', async function(req, res, next) {
+router.post('/login', async (req, res, next) => {
+  try {
+    const db = await dbConn;
 
-  const db = await dbConn;
+    const coll = db.collection("users");
 
-  const coll = db.collection("users");
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+    }
 
-  const user = {
-    username: req.body.username,
-    password: req.body.password,
+    const foundUser = await coll.findOne(user);
+
+    if(!foundUser) {
+      res.status(404);
+      res.send("");
+      return;
+    }
+
+    res.send({
+      id: foundUser._id,
+      username: foundUser.username,
+      token: generateAccessToken(foundUser.username),
+    });
+
+  } catch(err) {
+    next(err)
   }
-
-  const foundUser = await coll.findOne(user);
-
-  res.send({
-    id: foundUser._id,
-    username: foundUser.username,
-    token: generateAccessToken(foundUser.username),
-  });
 });
 
 module.exports = router;
